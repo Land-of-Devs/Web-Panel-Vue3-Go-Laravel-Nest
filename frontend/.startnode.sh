debfb5b56b3a54226ddfe8ee4d6ad9c89aa5f0d9
iferrer20 <iferreriestacio@gmail.com>
2021-11-18 19:18:43 +0100

    Vue container

---
+++ frontend/.startnode.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Perms
+uid=`stat -c '%u' .`
+gid=`stat -c '%g' .`
+
+sed -i -e "s/^node:x:[0-9]*:[0-9]*:/node:x:${uid}:${gid}:/" /etc/passwd
+chown ${uid}:${gid} /home/node
+
+# Install and run
+su node -c "id && npm install && npm run dev"
