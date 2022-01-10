c87df69a4397f6492fc6f0b4999236887b3bb0ca
iferrer20 <iferrer20@users.noreply.github.com>
2022-01-10 01:05:07 +0100

    Laravel: Product database factory (dummy)

---
+++ backend/laravel/database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Product;
+
+
+class ProductFactory extends Factory
+{
+
+    protected $model = Product::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'slug' => $this->faker->name(),
+            'name' => Str::random(4),
+            'description' => 'description product',
+            'price' => random_int(10, 10000),
+            'creator' => null,
+            'image' => 'dummy' . strval(random_int(1, 3)),
+            'status' => 'Complete'
+        ];
+    }
+
+}
+
